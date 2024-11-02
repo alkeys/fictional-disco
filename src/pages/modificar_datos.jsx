67008e4c3d938681b99422c140a953cb57d748c5
VladimirCM <cm19086@ues.edu.sv>
2024-11-02 11:32:35 -0600

    t

--- src/pages/modificar_datos.jsx
+++ src/pages/modificar_datos.jsx
@@ -73,7 +73,7 @@ export const ModificarDatos = () => {
     };
 
     return (
-        <div className="bg-red-700 pb-6 flex flex-col">
+        <div className="bg-red-700 pb-6 flex flex-col min-h-screen">
             <ButtonAtras />
             <div className="flex flex-col text-center px-10 py-4 bg-red-700">
                 <h1 className="text-5xl font-bold mb-6 text-white">Modificación de Datos</h1>
@@ -100,7 +100,7 @@ export const ModificarDatos = () => {
                     <p className="mt-3">&nbsp;</p>
                 )}
             </div>
-            <div className="flex flex-col text-center py-6 mx-3 px-3 md:mx-10 md:px-10 border rounded-lg bg-white pb-6">
+            <div className="flex flex-col text-center py-6 mx-3 px-3 md:mx-10 md:px-10 border rounded-lg bg-white pb-6 min-h-screen">
                 {seleccionCatalogo && documento && (
                     <BalanceGeneral
                         estado={data.find(item => item.name === seleccionCatalogo)}
