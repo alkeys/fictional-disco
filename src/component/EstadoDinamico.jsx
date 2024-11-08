2c9fe83b019bbc7c1d21ef262432c5a7684238d6
VladimirCM <cm19086@ues.edu.sv>
2024-11-08 08:50:13 -0600

    vista actualizada

--- src/component/EstadoDinamico.jsx
+++ src/component/EstadoDinamico.jsx
@@ -1,28 +1,48 @@
 import React from "react";
 
 const EstadoDinamico = ({ estado }) => {
+    const meses = [
+        "enero", "febrero", "marzo", "abril", "mayo", "junio",
+        "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+    ];
 
     return (
         <>
             {estado ? (
                 <table className="text-left">
                     <thead className="text-center font-bold">
-                        <tr>{estado.name}</tr>
-                        <tr>{estado.name_empresa}</tr>
-                        <tr>Del {estado.fecha.dia} de {estado.fecha.mes} del {estado.fecha.anio}</tr>
+                        <tr>
+                            <th colSpan="100%">
+                                {estado.name}
+                            </th>
+                        </tr>
+                        <tr>
+                            <th colSpan="100%">
+                                {estado.name_empresa}
+                            </th>
+                        </tr>
+                        <tr>
+                            <th colSpan="100%">
+                                Del {estado.fecha.dia} de {meses[estado.fecha.mes - 1]} del {estado.fecha.anio}
+                            </th>
+                        </tr>
                     </thead>
                     <tbody>
-                        {Object.keys(estado).splice(4).map((key1, index1) => (
+                        {Object.keys(estado).filter(key1 => !["id", "name", "name_empresa", "fecha"].includes(key1)).map((key1, index1) => (
                             <React.Fragment key={index1}>
                                 <tr>
                                     <td colSpan={3}>
-                                        <strong>{key1}</strong>
+                                        {estado.name === "Balance General" ? (
+                                            <strong>{key1}</strong>
+                                        ) : (
+                                            key1
+                                        )}
                                     </td>
                                     {estado[key1] && typeof estado[key1] === 'object' && Object.keys(estado[key1]).length > 0 ? (
-                                        <td></td>
+                                        <td>&nbsp;</td>
                                     ) : (
                                         <td>
-                                            ${estado[key1]}
+                                            ${Number(estado[key1]).toLocaleString()}
                                         </td>
                                     )}
                                 </tr>
@@ -30,34 +50,38 @@ const EstadoDinamico = ({ estado }) => {
                                     Object.keys(estado[key1]).map((key2, index2) => (
                                         <React.Fragment key={index2}>
                                             <tr>
-                                                <td colSpan={3}  className="px-4 py-2">
+                                                <td colSpan={3} className="px-4 py-2">
                                                     <strong>{key2}</strong>
                                                 </td>
                                                 {estado[key1][key2] && typeof estado[key1][key2] === 'object' && Object.keys(estado[key1][key2]).length > 0 ? (
-                                                    <td></td>
+                                                    <td>&nbsp;</td>
                                                 ) : (
                                                     <td className="text-right">
-                                                        <strong>${estado[key1][key2]}</strong>
+                                                        <strong>${Number(estado[key1][key2]).toLocaleString()}</strong>
                                                     </td>
                                                 )}
                                             </tr>
                                             {estado[key1][key2] && typeof estado[key1][key2] === 'object' && Object.keys(estado[key1][key2]).length > 0 && (
-                                                    Object.keys(estado[key1][key2]).map((key3, index3) => (
-                                                        <React.Fragment key={index3}>
-                                                            <tr>
-                                                                <td colSpan={3} className="px-8 py-2">
-                                                                    {key3}
-                                                                </td>
-                                                                {estado[key1][key2][key3] && typeof estado[key1][key2][key3] === 'object' && Object.keys(estado[key1][key2][key3]).length > 0 ? (
-                                                                    <td></td>
+                                                Object.keys(estado[key1][key2]).map((key3, index3) => (
+                                                    <React.Fragment key={index3}>
+                                                        <tr>
+                                                            <td colSpan={3} className="px-8 py-2">
+                                                                {["Total activos corrientes", "Total activos no corrientes", "Total pasivos corrientes", "Total pasivos no corrientes"].includes(key3) ? (
+                                                                    <strong>{key3}</strong>
                                                                 ) : (
-                                                                    <td className="text-right">
-                                                                        ${estado[key1][key2][key3]}
-                                                                    </td>
+                                                                    key3
                                                                 )}
-                                                            </tr>
-                                                        </React.Fragment>
-                                                    ))
+                                                            </td>
+                                                            {estado[key1][key2][key3] && typeof estado[key1][key2][key3] === 'object' && Object.keys(estado[key1][key2][key3]).length > 0 ? (
+                                                                <td></td>
+                                                            ) : (
+                                                                <td className="text-right">
+                                                                    ${Number(estado[key1][key2][key3]).toLocaleString()}
+                                                                </td>
+                                                            )}
+                                                        </tr>
+                                                    </React.Fragment>
+                                                ))
                                             )}
                                         </React.Fragment>
                                     ))
